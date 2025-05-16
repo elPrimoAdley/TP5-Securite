4d132cee9fc19ed44583f5348805716203ceeae8
elPrimoAdley <mbuguabradley2@gmail.com>
2025-05-16 13:35:42 -0400

    init projet

---
+++ WebOAuthServer/WebOAuthServer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebOAuthServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
